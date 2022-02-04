10afb08057457313b3d84d8c4eaea00bd90a268d
jyb <alex.ybjang@gmail.com>
2022-02-04 20:58:32 +0900

    유기동물 api 최신데이터 업데이트 (2021.08 ~ 2022.01)

--- src/main/java/ZooZoo/Controller/Board/BoardController.java
+++ src/main/java/ZooZoo/Controller/Board/BoardController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -42,8 +44,10 @@ public class BoardController {
     }
 
     // 상세페이지로
-    @GetMapping("/Board/Loss/LossBoardView")
-    public String goToLossBoardView() {
+    @GetMapping("/Board/Loss/LossBoardView/{ABDM_IDNTFY_NO}")
+    public String goToLossBoardView(Model model, @PathVariable("ABDM_IDNTFY_NO") String ABDM_IDNTFY_NO) {
+        ArrayList<LossDTO> lossDTOS = lossService.getlossboard(ABDM_IDNTFY_NO);
+        model.addAttribute("lossDTOS",lossDTOS);
         return "Board/Loss/LossBoardView";
     }
 
